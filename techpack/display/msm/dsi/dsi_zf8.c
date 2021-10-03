de30b9e34f0eb3f3109261df1f76379477bd78a2
Alexander Koskovich <akoskovich@pm.me>
2021-10-02 19:46:55 -0700

    treewide: Update to 30.12.112.22.
    Change-Id: I2ba8698d7078e1d85eeb7d9c6a6c92d5b88cff8d

--- techpack/display/msm/dsi/dsi_zf8.c
+++ techpack/display/msm/dsi/dsi_zf8.c
@@ -339,8 +339,13 @@ static void display_exit_idle_mode()
 	DSI_LOG("power_mode/aod_state are (%d, %d)\n",
 			g_display->panel->power_mode, g_display->panel->aod_state);
 	
-	if ((g_display->panel->power_mode != 1 && g_display->panel->power_mode != 2) && !g_display->panel->aod_state && !g_display->panel->has_enter_aod_before ) {
+	if ((g_display->panel->power_mode != 1 && g_display->panel->power_mode != 2) && !g_display->panel->aod_state) {
 		DSI_LOG("return display_exit_idle_mode!\n");
+		return;
+	}
+
+	if (!g_display->panel->has_enter_aod_before) {
+		DSI_LOG("has_enter_aod_before return display_exit_idle_mode!\n");
 		return;
 	}
 	
