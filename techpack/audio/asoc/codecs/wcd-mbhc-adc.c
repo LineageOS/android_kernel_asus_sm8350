39d528c5ebfd315135c92aa6c716a3e85242e45d
Alexander Koskovich <akoskovich@pm.me>
2022-01-05 20:27:12 -0700

    techpack: audio: import asus changes
    Change-Id: I48b2cef33a14ad0493871884f7d17f59d0b93971

--- techpack/audio/asoc/codecs/wcd-mbhc-adc.c
+++ techpack/audio/asoc/codecs/wcd-mbhc-adc.c
@@ -280,7 +280,8 @@ done:
 
 	return anc_mic_found;
 }
-
+/* ASUS_BSP +++ Fix OMTP headset issue */
+#if 0
 /* To determine if cross connection occurred */
 static int wcd_check_cross_conn(struct wcd_mbhc *mbhc)
 {
@@ -373,6 +374,8 @@ done:
 
 	return (plug_type == MBHC_PLUG_TYPE_GND_MIC_SWAP) ? true : false;
 }
+#endif
+/* ASUS_BSP --- Fix OMTP headset issue */
 
 static int wcd_mbhc_adc_get_spl_hs_thres(struct wcd_mbhc *mbhc)
 {
@@ -667,7 +670,7 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 	int ret = 0;
 	int spl_hs_count = 0;
 	int output_mv = 0;
-	int cross_conn;
+	int cross_conn = 0;/* ASUS_BSP +++ Fix OMTP headset issue */
 	int try = 0;
 	int hs_threshold, micbias_mv;
 
@@ -686,7 +689,7 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 
 	/* Check for cross connection */
 	do {
-		cross_conn = wcd_check_cross_conn(mbhc);
+		//cross_conn = wcd_check_cross_conn(mbhc);/* ASUS_BSP +++ Fix OMTP headset issue */
 		try++;
 	} while (try < mbhc->swap_thr);
 
@@ -784,7 +787,7 @@ correct_plug_type:
 		if ((output_mv <= hs_threshold) &&
 		    (!is_pa_on)) {
 			/* Check for cross connection*/
-			ret = wcd_check_cross_conn(mbhc);
+			//ret = wcd_check_cross_conn(mbhc); /* ASUS_BSP +++ Fix OMTP headset issue */
 			if (ret < 0)
 				continue;
 			else if (ret > 0) {
@@ -941,8 +944,16 @@ enable_supply:
 exit:
 	if (mbhc->mbhc_cb->mbhc_micbias_control &&
 	    !mbhc->micbias_enable)
-		mbhc->mbhc_cb->mbhc_micbias_control(component, MIC_BIAS_2,
+	{/* #ASUS_BSP +++ Enable MIC bias always if HEADSET inserted for ASUS Design */
+		if (plug_type == MBHC_PLUG_TYPE_HEADSET) {
+			mbhc->mbhc_cb->mbhc_micbias_control(component, MIC_BIAS_2,
+							    MICB_ENABLE);
+			mbhc->micbias_enable = true;
+		} else {/* #ASUS_BSP --- */
+			mbhc->mbhc_cb->mbhc_micbias_control(component, MIC_BIAS_2,
 						    MICB_DISABLE);
+		}
+	}
 
 	/*
 	 * If plug type is corrected from special headset to headphone,
