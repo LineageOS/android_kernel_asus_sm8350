f2eea6b099769aeeeaff524aee7e8397648f9096
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-09 17:05:25 +0200

    techpack: camera: import asus changes
    Change-Id: Idc7ff614cc9fde744f37361cf353a1c7ae01f9e6

--- techpack/camera/drivers/cam_sensor_module/cam_actuator/cam_actuator_dev.h
+++ techpack/camera/drivers/cam_sensor_module/cam_actuator/cam_actuator_dev.h
@@ -115,6 +115,8 @@ struct cam_actuator_ctrl_t {
 	struct cam_actuator_query_cap act_info;
 	struct actuator_intf_params bridge_intf;
 	uint32_t last_flush_req;
+	uint32_t lens_pos;
+	uint8_t debug_node_created;
 };
 
 /**
