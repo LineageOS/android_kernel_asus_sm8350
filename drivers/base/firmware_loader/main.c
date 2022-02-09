65d248d54370ea7ab7177b3793e8c7a9334031f6
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-09 15:16:13 +0200

    base: firmware_loader: redirect sake aw8697 firmware loading to /vendor/firmware
    Change-Id: Id9e2aea5f6f1eb732c33b693a1164e1554092967

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -511,6 +511,11 @@ fw_get_filesystem_firmware(struct device *device, struct fw_priv *fw_priv,
 			snprintf(path, PATH_MAX, "%s/%s", "/vendor/firmware", fw_priv->fw_name);
 #endif
 
+#if defined ASUS_SAKE_PROJECT
+		if (!strncmp(fw_priv->fw_name, "aw8697", 6))
+			snprintf(path, PATH_MAX, "%s/%s", "/vendor/firmware", fw_priv->fw_name);
+#endif
+
 		fw_priv->size = 0;
 		rc = kernel_read_file_from_path(path, &buffer, &size,
 						msize, id);
