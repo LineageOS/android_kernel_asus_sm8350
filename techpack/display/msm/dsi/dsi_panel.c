768f85df5ac8eb4d6d2eac6686d1aaf7bc594a81
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 15:07:38 +0200

    techpack: display: dsi_panel: define FOD HBM commands
    Change-Id: If910f52f209b52710a71d3211df786243369c362

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -1765,6 +1765,8 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-post-mode-switch-on-command",
 	"qcom,mdss-dsi-qsync-on-commands",
 	"qcom,mdss-dsi-qsync-off-commands",
+	"qcom,mdss-dsi-fod-hbm-on-command",
+	"qcom,mdss-dsi-fod-hbm-off-command",
 };
 
 const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
@@ -1791,6 +1793,8 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-post-mode-switch-on-command-state",
 	"qcom,mdss-dsi-qsync-on-commands-state",
 	"qcom,mdss-dsi-qsync-off-commands-state",
+	"qcom,mdss-dsi-fod-hbm-on-command-state",
+	"qcom,mdss-dsi-fod-hbm-off-command-state",
 };
 
 int dsi_panel_get_cmd_pkt_count(const char *data, u32 length, u32 *cnt)
