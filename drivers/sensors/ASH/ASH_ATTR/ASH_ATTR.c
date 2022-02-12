9760724576859ff7a823cb0214151884deb54c1c
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-02-11 20:40:30 -0500

    drivers/sensors: ASH: actually use debug print functions for debug stuff
    Using printk for any of this is not exactly ideal especially for sensors
    that poll at a very high rate such as the ambient light sensor. Switch
    everything to pr_debug to stop them from completely destroying the dmesg
    with useless information.
    
    Signed-off-by: Sourajit Karmakar <sourajit.karmakar@gmail.com>
    Change-Id: Ie6ccb4c676dd70200ae606149dfb21e83351ab97

--- drivers/sensors/ASH/ASH_ATTR/ASH_ATTR.c
+++ drivers/sensors/ASH/ASH_ATTR/ASH_ATTR.c
@@ -33,12 +33,12 @@ static struct class *g_property_class = NULL;
 
 #undef dbg
 #ifdef ASH_ATTR_DEBUG
-	#define dbg(fmt, args...) printk(KERN_DEBUG "[%s]"fmt,MODULE_NAME,##args)
+	#define dbg(fmt, args...) pr_debug(KERN_DEBUG "[%s]"fmt,MODULE_NAME,##args)
 #else
 	#define dbg(fmt, args...)
 #endif
-#define log(fmt, args...) printk(KERN_INFO "[%s]"fmt,MODULE_NAME,##args)
-#define err(fmt, args...) printk(KERN_ERR "[%s]"fmt,MODULE_NAME,##args)
+#define log(fmt, args...) pr_debug("[%s]"fmt,MODULE_NAME,##args)
+#define err(fmt, args...) pr_debug("[%s]"fmt,MODULE_NAME,##args)
 
 static int ASH_class_open(struct inode * inode, struct file * file)
 {
