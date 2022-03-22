b7b6f6fc955f9566d72344bd14a6c39d02ed0415
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-22 20:09:30 +0200

    Revert "input: touchscreen: focaltech_touch_v3_3: expose double click gesture attr for sake"
    This reverts commit 5e282faaedc4b3606759a19e8e2b39b0c177ba3e.
    
    Change-Id: I5151c6e73f03a11470a82b909d067229b295fccc

--- drivers/input/touchscreen/focaltech_touch_v3_3/focaltech_ex_fun.c
+++ drivers/input/touchscreen/focaltech_touch_v3_3/focaltech_ex_fun.c
@@ -1187,33 +1187,6 @@ static ssize_t asus_ex_proc_fpxy_read(struct file *file, char __user *buf,
 	return simple_read_from_buffer(buf, count, ppos, str, len);
 }
 
-static ssize_t asus_gesture_proc_dclick_read(struct file *file,
-					     char __user *buf, size_t count,
-					     loff_t *ppos)
-{
-	char str[3];
-	int len;
-
-	len = snprintf(str, sizeof(str), "%u\n",
-		       fts_data->enabled_gestures[GESTURE_TYPE_DOUBLECLICK]);
-
-	return simple_read_from_buffer(buf, count, ppos, str, len);
-}
-
-static ssize_t asus_gesture_proc_dclick_write(struct file *filp,
-					      const char __user *buf,
-					      size_t len, loff_t *off)
-{
-	char str[1];
-
-	if (copy_from_user(str, buf, sizeof(str)))
-		return -EFAULT;
-
-	fts_gesture_set(fts_data, GESTURE_TYPE_DOUBLECLICK, str[0] != '0');
-
-	return len;
-}
-
 static ssize_t fts_high_report_rate_show(struct device *dev,
 					 struct device_attribute *attr,
 					 char *buf)
@@ -1298,11 +1271,6 @@ static struct attribute_group fts_attribute_group = { .attrs = fts_attributes };
 static struct file_operations asus_ex_proc_fpxy_ops = {
 	.read = asus_ex_proc_fpxy_read,
 };
-
-static struct file_operations asus_gesture_proc_dclick_ops = {
-	.write = asus_gesture_proc_dclick_write,
-	.read = asus_gesture_proc_dclick_read,
-};
 #endif
 
 int fts_create_sysfs(struct fts_ts_data *ts_data)
@@ -1320,7 +1288,6 @@ int fts_create_sysfs(struct fts_ts_data *ts_data)
 
 #if defined ASUS_SAKE_PROJECT
 	proc_create("driver/fp_xy", 0777, NULL, &asus_ex_proc_fpxy_ops);
-	proc_create("driver/dclick", 0777, NULL, &asus_gesture_proc_dclick_ops);
 #endif
 
 	return ret;
@@ -1332,7 +1299,6 @@ int fts_remove_sysfs(struct fts_ts_data *ts_data)
 
 #if defined ASUS_SAKE_PROJECT
 	remove_proc_entry("driver/fp_xy", NULL);
-	remove_proc_entry("driver/dclick", NULL);
 #endif
 
 	return 0;
