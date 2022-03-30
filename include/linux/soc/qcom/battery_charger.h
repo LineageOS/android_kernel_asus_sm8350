f1e74da8fbd2e881f05b207b5ae66ca46223bf30
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-30 15:09:18 +0300

    power: supply: qti: add battery charging notifier
    Change-Id: I98b86855a9833ba9aec7ba7eade1dffaa2fb7bea

--- include/linux/soc/qcom/battery_charger.h
+++ include/linux/soc/qcom/battery_charger.h
@@ -6,6 +6,8 @@
 #ifndef _BATTERY_CHARGER_H
 #define _BATTERY_CHARGER_H
 
+#include <linux/notifier.h>
+
 enum battery_charger_prop {
 	BATTERY_RESISTANCE,
 	BATTERY_CHARGER_PROP_MAX,
@@ -14,6 +16,8 @@ enum battery_charger_prop {
 #if IS_ENABLED(CONFIG_QTI_BATTERY_CHARGER)
 int qti_battery_charger_get_prop(const char *name,
 				enum battery_charger_prop prop_id, int *val);
+void qti_charge_register_notify(struct notifier_block *nb);
+void qti_charge_unregister_notify(struct notifier_block *nb);
 #else
 static inline int
 qti_battery_charger_get_prop(const char *name,
@@ -21,6 +25,8 @@ qti_battery_charger_get_prop(const char *name,
 {
 	return -EINVAL;
 }
+void qti_charge_register_notify(struct notifier_block *nb) {}
+void qti_charge_unregister_notify(struct notifier_block *nb) {}
 #endif
 
 #endif
