154b1ff47b87e3bc402cdaedc630de11770915b3
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-30 17:04:05 +0300

    power: supply: qti: implement usb thermal work
    Change-Id: I8e4862bf43738f0213e14ef79b91cdeedf8d38dc

--- drivers/power/supply/asus_battery_charger.h
+++ drivers/power/supply/asus_battery_charger.h
@@ -4,8 +4,10 @@
 #define _ASUS_BATTERY_CHARGER_H
 
 #include <linux/gpio/consumer.h>
+#include <linux/iio/consumer.h>
 #include <linux/soc/qcom/battery_charger.h>
 #include <linux/soc/qcom/pmic_glink.h>
+#include <linux/workqueue.h>
 
 struct asus_battery_chg {
 	bool				initialized;
@@ -13,6 +15,8 @@ struct asus_battery_chg {
 	struct pmic_glink_client	*client;
 	struct gpio_desc		*otg_switch;
 	bool				usb_present;
+	struct iio_channel		*temp_chan;
+	struct delayed_work		usb_thermal_work;
 };
 
 int asus_battery_charger_init(struct asus_battery_chg *abc);
