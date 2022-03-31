449e48de04ca19cdaf363692961e3f82165c4062
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-31 19:51:36 +0300

    power: supply: qti: add asus battery charger stub
    Change-Id: I85d9017ca191f13d43a18b68b507c3a45e53e17c

---
+++ drivers/power/supply/asus_battery_charger.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include "asus_battery_charger.h"
+
+int asus_battery_charger_init(struct asus_battery_chg *abc)
+{
+	return 0;
+}
+
+int asus_battery_charger_deinit(struct asus_battery_chg *abc)
+{
+	return 0;
+}
