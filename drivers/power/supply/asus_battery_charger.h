180628bf9771bea69692a589c636095a15017b91
Cosmin Tanislav <demonsingur@gmail.com>
2022-04-01 01:06:19 +0300

    power: supply: qti: implement battery safety algorithm
    Change-Id: I2d35a81f0bd6d417f70604cd195e87d5ad22e300

--- drivers/power/supply/asus_battery_charger.h
+++ drivers/power/supply/asus_battery_charger.h
@@ -11,15 +11,31 @@
 #include <linux/pm_wakeup.h>
 #include <linux/workqueue.h>
 
+struct cycle_count_data {
+	int				magic;
+	int				cycle_count;
+	unsigned long			battery_total_time;
+	unsigned long			high_vol_total_time;
+	unsigned long			high_temp_total_time;
+	unsigned long			high_temp_vol_time;
+	u32				reload_condition;
+};
+
 struct asus_battery_chg {
 	bool				initialized;
 	struct class			asuslib_class;
+	unsigned long			last_battery_total_time;
+	unsigned long			last_high_temp_time;
+	unsigned long			last_high_vol_time;
+	struct cycle_count_data		cycle_count_data;
+	bool				cycle_count_data_initialized;
 	struct battery_chg_dev		*bcdev;
 	struct power_supply		*batt_psy;
 	struct pmic_glink_client	*client;
 	struct wakeup_source		*slowchg_ws;
 	struct gpio_desc		*otg_switch;
 	struct drm_panel		*panel;
+	struct notifier_block		reboot_notif;
 	struct notifier_block		drm_notif;
 	bool				panel_on;
 	struct delayed_work		panel_state_work;
@@ -38,6 +54,7 @@ struct asus_battery_chg {
 	struct delayed_work		jeita_prechg_work;
 	struct delayed_work		jeita_cc_work;
 	struct delayed_work		full_cap_monitor_work;
+	struct delayed_work		battery_safety_work;
 };
 
 int asus_battery_charger_init(struct asus_battery_chg *abc);
