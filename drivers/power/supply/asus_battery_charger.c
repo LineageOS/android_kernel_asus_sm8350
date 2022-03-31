05434e67587c0e0338aa48e795f05eb6c5cc7307
Cosmin Tanislav <demonsingur@gmail.com>
2022-04-01 01:06:19 +0300

    power: supply: qti: implement battery safety algorithm
    Change-Id: I2d35a81f0bd6d417f70604cd195e87d5ad22e300

--- drivers/power/supply/asus_battery_charger.c
+++ drivers/power/supply/asus_battery_charger.c
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/of.h>
+#include <linux/power_supply.h>
+#include <linux/reboot.h>
+#include <linux/syscalls.h>
 
 #include "asus_battery_charger.h"
 
@@ -27,6 +30,8 @@
 
 #define OEM_THERMAL_THRESHOLD		23
 
+#define OEM_FV				25
+
 #define JETA_NONE			0
 #define JETA_CV				4
 
@@ -38,6 +43,33 @@
 #define OEM_USB_PRESENT			0x2108
 #define OEM_WORK_EVENT_REQ		0x2110
 #define OEM_JEITA_CC_STATE_REQ		0x2112
+
+#ifdef CONFIG_MACH_ASUS_SAKE
+#define INIT_FV 4450
+#else
+#define INIT_FV 4360
+#endif
+
+#define HIGH_TEMP			350
+#define HIGHER_TEMP			450
+#define FULL_CAPACITY_VALUE		100
+
+#define MONTH_SECS			(1 * 30 * 24 * 60 * 60)
+
+#define BATTERY_USE_TIME_CONDITION1	MONTH_SECS
+#define BATTERY_USE_TIME_CONDITION2	(3 * MONTH_SECS)
+#define BATTERY_USE_TIME_CONDITION3	(12 * MONTH_SECS)
+#define BATTERY_USE_TIME_CONDITION4	(18 * MONTH_SECS)
+
+#define HIGH_TEMP_TIME_CONDITION1	(6 * MONTH_SECS)
+#define HIGH_TEMP_TIME_CONDITION2	(12 * MONTH_SECS)
+
+#define HIGH_VOL_TIME_CONDITION1	(6 * MONTH_SECS)
+#define HIGH_VOL_TIME_CONDITION2	(12 * MONTH_SECS)
+
+#define CYCLE_COUNT_DATA_MAGIC		0x85
+#define CYCLE_COUNT_FILE_NAME		"/batinfo/.bs"
+#define BATTERY_SAFETY_UPGRADE_TIME	(1 * 60)
 
 #define dwork_to_abc(work, member) \
 	container_of(to_delayed_work(work), struct asus_battery_chg, member)
@@ -278,6 +310,312 @@ static void panel_state_worker(struct work_struct *work)
 			tmp, rc);
 }
 
+static int file_op(const char *filename, loff_t offset, char *buf, int length,
+		   bool write)
+{
+	mm_segment_t old_fs = get_fs();
+	int fd;
+
+	set_fs(KERNEL_DS);
+
+	if (write)
+		fd = ksys_open(filename, O_RDWR | O_CREAT | O_SYNC, 0666);
+	else
+		fd = ksys_open(filename, O_RDONLY | O_CREAT | O_SYNC, 0666);
+
+	if (fd < 0) {
+		pr_err("Failed to open file %s, rc=%d\n", filename, fd);
+		return fd;
+	}
+
+	ksys_lseek(fd, offset, SEEK_SET);
+	if (write) {
+		ksys_write(fd, buf, length);
+		ksys_sync();
+	} else {
+		ksys_read(fd, buf, length);
+	}
+
+	ksys_close(fd);
+
+	set_fs(old_fs);
+
+	return 0;
+}
+
+static void write_cycle_count_data(struct asus_battery_chg *abc)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	int rc;
+
+	rc = file_op(CYCLE_COUNT_FILE_NAME, 0, (char *)&abc->cycle_count_data,
+		     sizeof(abc->cycle_count_data), true);
+	if (rc < 0)
+		dev_err(dev, "Failed to write cycle count data, rc=%d\n", rc);
+}
+
+static void set_full_charging_voltage(struct asus_battery_chg *abc)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	u32 tmp;
+	int rc;
+
+	switch (abc->cycle_count_data.reload_condition) {
+#if defined(CONFIG_MACH_ASUS_SAKE)
+	case 1:
+		tmp = INIT_FV - 20;
+		break;
+	case 2:
+		tmp = INIT_FV - 50;
+		break;
+	case 3:
+		tmp = INIT_FV - 100;
+		break;
+	case 4:
+		tmp = INIT_FV - 150;
+		break;
+#elif defined(CONFIG_MACH_ASUS_VODKA)
+	case 1:
+		tmp = INIT_FV;
+		break;
+	case 2:
+		tmp = INIT_FV;
+		break;
+	case 3:
+		tmp = INIT_FV - 50;
+		break;
+	case 4:
+		tmp = INIT_FV - 100;
+		break;
+#endif
+	default:
+		return;
+	}
+
+	rc = write_property_id_oem(abc, OEM_FV, &tmp, 1);
+	if (rc)
+		dev_err(dev, "Failed to write full voltage %u, rc=%d\n",
+			tmp, rc);
+}
+
+static int init_cycle_count_data(struct asus_battery_chg *abc)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct cycle_count_data buf;
+	int rc = 0;
+
+	rc = file_op(CYCLE_COUNT_FILE_NAME, 0, (char *)&buf, sizeof(buf),
+		     false);
+	if (rc < 0) {
+		dev_err(dev, "Failed to read cycle data, rc=%d\n", rc);
+		return rc;
+	}
+
+	if (buf.magic != CYCLE_COUNT_DATA_MAGIC) {
+		write_cycle_count_data(abc);
+		return -EINVAL;
+	}
+
+	memcpy(&abc->cycle_count_data, &buf, sizeof(buf));
+	abc->cycle_count_data_initialized = true;
+
+	set_full_charging_voltage(abc);
+
+	return 0;
+}
+
+static void judge_reload_condition(struct asus_battery_chg *abc)
+{
+	unsigned long battery_total_time =
+		abc->cycle_count_data.battery_total_time;
+	unsigned long high_temp_time =
+		abc->cycle_count_data.high_temp_total_time;
+	unsigned long high_vol_time =
+		abc->cycle_count_data.high_vol_total_time;
+	int reload_condition =
+		abc->cycle_count_data.reload_condition;
+
+	if (reload_condition >= 4)
+		return;
+
+	if (battery_total_time >= BATTERY_USE_TIME_CONDITION4) {
+		reload_condition = 4;
+		goto done;
+	} else if (battery_total_time >= BATTERY_USE_TIME_CONDITION3 &&
+		   battery_total_time < BATTERY_USE_TIME_CONDITION4) {
+		reload_condition = 3;
+	}
+#ifdef CONFIG_MACH_ASUS_SAKE
+	else if (battery_total_time >= BATTERY_USE_TIME_CONDITION2 &&
+		 battery_total_time < BATTERY_USE_TIME_CONDITION3) {
+		reload_condition = 2;
+	} else if (battery_total_time >= BATTERY_USE_TIME_CONDITION1 &&
+		   battery_total_time < BATTERY_USE_TIME_CONDITION2) {
+		reload_condition = 1;
+	}
+#endif
+
+	if (high_temp_time >= HIGH_TEMP_TIME_CONDITION2) {
+		reload_condition = 4;
+		goto done;
+	} else if (high_temp_time >= HIGH_TEMP_TIME_CONDITION1 &&
+		   high_temp_time < HIGH_TEMP_TIME_CONDITION2) {
+		reload_condition = 3;
+	}
+
+	if (high_vol_time >= HIGH_VOL_TIME_CONDITION2) {
+		reload_condition = 4;
+		goto done;
+	} else if (high_vol_time >= HIGH_VOL_TIME_CONDITION1 &&
+		   high_vol_time < HIGH_VOL_TIME_CONDITION2) {
+		reload_condition = 3;
+	}
+
+done:
+	if (reload_condition == abc->cycle_count_data.reload_condition)
+		return;
+
+	abc->cycle_count_data.reload_condition = reload_condition;
+
+	set_full_charging_voltage(abc);
+}
+
+static void calculate_total_time(struct asus_battery_chg *abc)
+{
+	unsigned long temp_time;
+	struct timespec64 now;
+
+	ktime_get_coarse_real_ts64(&now);
+
+	if (!abc->last_battery_total_time) {
+		abc->last_battery_total_time = now.tv_sec;
+		return;
+	}
+
+	temp_time = now.tv_sec - abc->last_battery_total_time;
+	if (temp_time > 0)
+		abc->cycle_count_data.battery_total_time += temp_time;
+
+	abc->last_battery_total_time = now.tv_sec;
+}
+
+static void calculate_high_vol_time(struct asus_battery_chg *abc)
+{
+	unsigned long temp_time;
+	struct timespec64 now;
+
+	ktime_get_coarse_real_ts64(&now);
+
+	if (!abc->last_high_vol_time) {
+		abc->last_high_vol_time = now.tv_sec;
+		return;
+	}
+
+	temp_time = now.tv_sec - abc->last_high_vol_time;
+	if (temp_time > 0)
+		abc->cycle_count_data.high_vol_total_time += temp_time;
+
+	abc->last_high_vol_time = now.tv_sec;
+}
+
+static void calculate_high_temp_time(struct asus_battery_chg *abc)
+{
+	unsigned long temp_time;
+	struct timespec64 now;
+
+	ktime_get_coarse_real_ts64(&now);
+
+	if (!abc->last_high_temp_time) {
+		abc->last_high_temp_time = now.tv_sec;
+		return;
+	}
+
+	temp_time = now.tv_sec - abc->last_high_temp_time;
+	if (temp_time > 0)
+		abc->cycle_count_data.high_temp_total_time += temp_time;
+
+	abc->last_high_temp_time = now.tv_sec;
+}
+
+static void update_safety_data(struct asus_battery_chg *abc)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	union power_supply_propval prop = {};
+	int capacity;
+	int temp;
+	int rc;
+
+	if (!abc->cycle_count_data_initialized) {
+		rc = init_cycle_count_data(abc);
+		if (rc < 0) {
+			dev_err(dev, "Failed to read cycle count data, rc=%d\n",
+				rc);
+			return;
+		}
+	}
+
+	rc = power_supply_get_property(abc->batt_psy, POWER_SUPPLY_PROP_TEMP,
+				       &prop);
+	if (rc) {
+		dev_err(dev, "Failed to get battery temp, rc=%d\n", rc);
+		return;
+	}
+	temp = prop.intval;
+
+	rc = power_supply_get_property(abc->batt_psy, POWER_SUPPLY_PROP_CAPACITY,
+				       &prop);
+	if (rc) {
+		dev_err(dev, "Failed to get battery capacity, rc=%d\n", rc);
+		return;
+	}
+	capacity = prop.intval;
+
+	calculate_total_time(abc);
+
+	if (capacity == FULL_CAPACITY_VALUE)
+		calculate_high_vol_time(abc);
+	else
+		abc->last_high_vol_time = 0;
+
+	if (temp >= HIGHER_TEMP)
+		calculate_high_temp_time(abc);
+	else
+		abc->last_high_temp_time = 0;
+
+	judge_reload_condition(abc);
+
+	write_cycle_count_data(abc);
+}
+
+static void battery_safety_worker(struct work_struct *work)
+{
+	struct asus_battery_chg *abc = dwork_to_abc(work, battery_safety_work);
+
+	update_safety_data(abc);
+
+	schedule_delayed_work(&abc->battery_safety_work,
+			      BATTERY_SAFETY_UPGRADE_TIME * HZ);
+}
+
+static int reboot_notifier_callback(struct notifier_block *self,
+				    unsigned long event, void *ptr)
+{
+	struct asus_battery_chg *abc = container_of(self,
+						    struct asus_battery_chg,
+						    reboot_notif);
+
+	switch (event) {
+	case SYS_RESTART:
+	case SYS_POWER_OFF:
+		write_cycle_count_data(abc);
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
 static int drm_notifier_callback(struct notifier_block *self,
 				 unsigned long event, void *data)
 {
@@ -476,6 +814,8 @@ int asus_battery_charger_init(struct asus_battery_chg *abc)
 	int rc;
 	int i;
 
+	abc->cycle_count_data.magic = CYCLE_COUNT_DATA_MAGIC;
+
 	count = of_count_phandle_with_args(node, "panel", NULL);
 	for (i = 0; i < count; i++) {
 		panel_node = of_parse_phandle(node, "panel", i);
@@ -577,6 +917,11 @@ int asus_battery_charger_init(struct asus_battery_chg *abc)
 	INIT_DELAYED_WORK(&abc->jeita_rule_work, jeita_rule_worker);
 	INIT_DELAYED_WORK(&abc->jeita_prechg_work, jeita_prechg_worker);
 	INIT_DELAYED_WORK(&abc->jeita_cc_work, jeita_cc_worker);
+
+	abc->reboot_notif.notifier_call = reboot_notifier_callback;
+	INIT_DELAYED_WORK(&abc->battery_safety_work, battery_safety_worker);
+	register_reboot_notifier(&abc->reboot_notif);
+	schedule_delayed_work(&abc->battery_safety_work, 30 * HZ);
 
 	abc->initialized = true;
 
