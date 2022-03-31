bca135337b6780e7c11c1b05cfa052d77009ab5c
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-31 21:00:38 +0300

    power: supply: qti: register charging notifier
    Change-Id: I742f5debe6a57f07e6c7a3c4bd9f9f8d36d99abd

--- drivers/power/supply/asus_battery_charger.h
+++ drivers/power/supply/asus_battery_charger.h
@@ -17,6 +17,8 @@ struct asus_battery_chg {
 	bool				usb_present;
 	struct iio_channel		*temp_chan;
 	struct delayed_work		usb_thermal_work;
+	struct notifier_block		usb_online_notifier;
+	bool				usb_online;
 };
 
 int asus_battery_charger_init(struct asus_battery_chg *abc);
