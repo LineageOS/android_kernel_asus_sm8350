68e641c1d16502666949aa9208a582ea0b4d8681
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-31 21:36:33 +0300

    power: supply: qti: handle jeita cc state request notifications
    Change-Id: Ie308799ea947cd1e2cf32d6f325ee65129a0b626

--- drivers/power/supply/asus_battery_charger.h
+++ drivers/power/supply/asus_battery_charger.h
@@ -25,6 +25,7 @@ struct asus_battery_chg {
 	int				thermal_threshold;
 	struct delayed_work		panel_check_work;
 	struct delayed_work		thermal_policy_work;
+	int				jeita_cc_state;
 	struct delayed_work		jeita_prechg_work;
 	struct delayed_work		jeita_cc_work;
 };
