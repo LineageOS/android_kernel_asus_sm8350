9da73fca66f7bb253f273403c79b9c068454ebe0
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-31 20:26:15 +0300

    power: supply: qti: add pmic glink client
    Change-Id: I1bb1ec7e95a286ebd2ae51300d77e51b0fd552bc

--- drivers/power/supply/asus_battery_charger.h
+++ drivers/power/supply/asus_battery_charger.h
@@ -4,9 +4,12 @@
 #define _ASUS_BATTERY_CHARGER_H
 
 #include <linux/soc/qcom/battery_charger.h>
+#include <linux/soc/qcom/pmic_glink.h>
 
 struct asus_battery_chg {
+	bool				initialized;
 	struct battery_chg_dev		*bcdev;
+	struct pmic_glink_client	*client;
 };
 
 int asus_battery_charger_init(struct asus_battery_chg *abc);
