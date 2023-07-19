aea72ad342f5b37f0c22be10263c10d5537abbcf
Davide Garberi <dade.garberi@gmail.com>
2023-07-19 18:23:39 +0200

    techpack: display: Add a variable to mark if hbm is manually enabled
    Change-Id: I75e1c754ad3072d23f25024ed6bfa63009039613

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -280,6 +280,7 @@ struct dsi_panel {
 	u8 fod_dim_alpha;
 	bool hbm_enabled;
 	bool fod_ui;
+	bool manual_hbm_enabled;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
